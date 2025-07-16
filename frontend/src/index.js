c1300359841fe77f14b11bc024fb361c6ba70935
PRACHI PARAB <prachiparab459@gmail.com>
2025-07-16 22:46:12 +0530

    Initial full project push with frontend code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './landingPage/home/HomePage';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <div>
-    <h1>HELLO</h1>
-   </div>
+    <HomePage/>   
   </React.StrictMode>
 );
 
